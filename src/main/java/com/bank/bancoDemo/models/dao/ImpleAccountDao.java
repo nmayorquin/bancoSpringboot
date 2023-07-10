38c386618929740c436696e12532fdb3ac290155
naty <natymayorn@gmail.com>
2023-07-09 20:39:07 -0500

    ejercicio sencillo, un post -dos get

--- src/main/java/com/bank/bancoDemo/models/dao/ImpleAccountDao.java
+++
@@ -1,8 +0,0 @@
-package com.bank.bancoDemo.models.dao;
-
-import com.bank.bancoDemo.models.entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImpleAccountDao extends JpaRepository<Account, Long> {
-
-}
